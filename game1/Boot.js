e063fa2c0795fdb744782dca4bff7fca80c73a1e
carl <carl.last@gmail.com>
2018-03-27 22:02:30 +0100

    Boot state

---
+++ game1/Boot.js
@@ -0,0 +1,17 @@
+var BunnyDefender = {};
+BunnyDefender.Boot = function (game) {};
+BunnyDefender.Boot.prototype = {
+    preload: function () {}
+    , create: function () {
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 270;
+        this.scale.minHeight = 480;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.stage.setScreenSize(true);
+        this.input.addPointer();
+        this.stage.backgroundColor = '#171642';
+    }
+}
